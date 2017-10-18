0014202bde131821a05ceab8daf9967be92ec131
TomMGrello <tommgrello@gmail.com>
2017-10-18 14:22:52 -0400

    Ajax stuff

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def main():
 	return render_template("index.html")
+
+@app.route("/showPermissions")
+def showPermissions():
+	return render_template("showPermissions.html");
+
+@app.route("/permissions",methods=['POST'])
+def permissions():
+	print('i am here')
+	username = request.form['usernameInput']
+	if username:
+		return json.dumps({'html':'<span>' + username + '</span>'})
+	return 'NOTHING!!!'
 
 if __name__ == "__main__":
     app.debug = True
---
+++ FlaskApp/static/js/getPermissions.js
@@ -0,0 +1,15 @@
+  $(function() {
+    $('#signin').click(function() {
+        $.ajax({
+            url: '/permissions',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
