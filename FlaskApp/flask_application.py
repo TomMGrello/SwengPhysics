5a1f255ae07df88879e111ba8a45d1e319ee287a
Grello <grellot6@rowan.edu>
2017-10-26 13:52:33 -0400

    Hopefully actually fixed the random crashing now. The errors were being thrown because the connection to the MySQL database was closed (probably by some hiccup in the EC2 server). The connections are now reestablished on each query.

--- FlaskApp/flask_application.py
+++ FlaskApp/flask_application.py
@@ -6,13 +6,12 @@ flask_application = Flask(__name__)
 mysql = MySQL()
 
 flask_application.config['MYSQL_DATABASE_USER'] = 'root'
-flask_application.config['MYSQL_DATABASE_PASSWORD'] = '0924'
+flask_application.config['MYSQL_DATABASE_PASSWORD'] = 'rowanphysicssweng'
 flask_application.config['MYSQL_DATABASE_DB'] = 'permissions'
 flask_application.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(flask_application)
 
 conn = mysql.connect()
-cursor = conn.cursor()
 
 @flask_application.route("/")
 def main():
@@ -24,31 +23,43 @@ def showPermissions():
 
 @flask_application.route("/storeUsername")
 def storeUsername():
+	result = "No user found"
+	cursor = conn.cursor()
+	
 	username = request.args.get('user','N/A')
 	session['username'] = username
 	cursor.callproc('sp_get_banner_id',[username])
+	
 	banner_id = cursor.fetchall()[0][0] #data is returned as ((<banner_id>,),) for some reason. So this extracts it.
 	print banner_id
 	session['banner_id'] = banner_id
+	
 	if username:
 		print username
-		return jsonify(result='Username is: ' + username)
-	return 'No user found'
+		result = jsonify(result='Username is: ' + username)
+		
+	cursor.close()
+	return result
 
 @flask_application.route("/permissions",methods=['GET'])
 def permissions():
+	cursor = conn.cursor()
+	result = "No banner id"
 	banner_id = session['banner_id']
+	
 	print 'BANNER FOR RETREIVAL: ' + str(banner_id)
+	
 	if banner_id:
 		cursor.callproc('sp_get_permissions',[banner_id])
 		perms = cursor.fetchall()
 		print perms
-		return jsonify(result=perms)
-	else:
-		return "NO BANNER ID"
+		result = jsonify(result=perms)
+	
+	cursor.close()
+	return result
 
 if __name__ == "__main__":
 	flask_application.debug = True
 	flask_application.secret_key = 'rowanphysicssweng'
-#	flask_application.run(host=os.getenv('LISTEN', '0.0.0.0'),threaded=True)
+	flask_application.run(host=os.getenv('LISTEN', '0.0.0.0'),threaded=True)
 	flask_application.run(threaded=True)
