70acfb18f1f347a55f49478f7c583724c617de91
00Bren00 <ringelb8@students.rowan.edu>
2017-10-26 16:26:18 -0400

    added comments for things needed to be done for personal dev work, and for changing to push work

--- FlaskApp/flask_application.py
+++ FlaskApp/flask_application.py
@@ -6,7 +6,7 @@ flask_application = Flask(__name__)
 mysql = MySQL()
 
 flask_application.config['MYSQL_DATABASE_USER'] = 'root'
-flask_application.config['MYSQL_DATABASE_PASSWORD'] = 'rowanphysicssweng'
+flask_application.config['MYSQL_DATABASE_PASSWORD'] = 'rowanphysicssweng'   # todo, change back to rowanphysicssweng for push, change to personal password for dev work
 flask_application.config['MYSQL_DATABASE_DB'] = 'permissions'
 flask_application.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(flask_application)
@@ -61,5 +61,5 @@ def permissions():
 if __name__ == "__main__":
 	flask_application.debug = True
 	flask_application.secret_key = 'rowanphysicssweng'
-	flask_application.run(host=os.getenv('LISTEN', '0.0.0.0'),threaded=True)
+	flask_application.run(host=os.getenv('LISTEN', '0.0.0.0'),threaded=True) # todo uncomment this line before push, comment before dev work
 	flask_application.run(threaded=True)
