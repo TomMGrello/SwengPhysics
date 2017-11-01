4bc5535d6988c75122007e268a6883260f0ff5c7
bellr3 <31863394+bellr3@users.noreply.github.com>
2017-11-01 15:15:48 -0400

    removed the auto increment as field in sp

--- database_init/sp_add_user_request.sql
+++ database_init/sp_add_user_request.sql
@@ -2,7 +2,6 @@ USE permissions;
 -- add user request stored procedure 
 DELIMITER $$
 CREATE PROCEDURE `sp_add_user_request`(
-	IN p_user_request_id int(36),
     IN p_first_name VARCHAR(45),
     IN p_last_name VARCHAR(45),
     IN p_banner_id int(9),
@@ -16,18 +15,16 @@ BEGIN
 	ELSE
 
 	REPLACE into user_requests (
-		user_request_id,
         first_name,
         last_name,
-		banner_id,
-		role
-		) values (
-		p_user_request_id,
-		p_first_name,
+	banner_id,
+	role
+	) values (
+	p_first_name,
         p_last_name,
         p_banner_id,
-		p_role
-		);
+	p_role
+	);
 	END IF;
 
 END $$
