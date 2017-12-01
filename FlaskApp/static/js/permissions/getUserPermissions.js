2216a90c285de9977a1a8fbd60516ab3df0f5efa
Connor Snee <snee0614@gmail.com>
2017-12-01 11:57:24 -0500

    work

--- FlaskApp/static/js/permissions/getUserPermissions.js
+++ FlaskApp/static/js/permissions/getUserPermissions.js
@@ -3,7 +3,9 @@ var getUserPermissions = function() {
         var data_array = data.result;
         console.log(data_array);
         var table = document.getElementById('tablebody');
-
+        for (var curr_item = 0; curr_item < data_array.length; curr_item++) {
+            
+        }
     });
     $.getJSON('/allUserPermissions', function(data){
         var data_array = data.result;
